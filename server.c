b662a57fb82a1f6210b7812007a091aea37d9b43
elkade <draganl@student.mini.pw.edu.pl>
2015-05-11 20:04:20 +0200

    first commit

---
+++ server.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <time.h>
+
+struct service{
+	char name[16];
+	char host[16];
+	uint16_t port;
+};
+
+
+int main(int argc, char** argv){
+	
+	
+	exit(EXIT_SUCCESS);
+}
